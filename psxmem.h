d61ade33ec5e759826fd946dbc99b2fb6c6d3f19
Mystro256 <alexjnewt@hotmail.com>
2016-10-25 22:17:33 -0400

    OR's instead of Add's
    OR's are cheaper and can be optimized further

--- psxmem.h
+++ psxmem.h
@@ -27,8 +27,8 @@
 #define _SWAP16(b) ((((unsigned char*)&(b))[0]&0xff) | (((unsigned char*)&(b))[1]&0xff)<<8)
 #define _SWAP32(b) ((((unsigned char*)&(b))[0]&0xff) | ((((unsigned char*)&(b))[1]&0xff)<<8) | ((((unsigned char*)&(b))[2]&0xff)<<16) | (((unsigned char*)&(b))[3]<<24))
 
-#define SWAP16(v) ((((v)&0xff00)>>8) +(((v)&0xff)<<8))
-#define SWAP32(v) ((((v)&0xff000000ul)>>24) + (((v)&0xff0000ul)>>8) + (((v)&0xff00ul)<<8) +(((v)&0xfful)<<24))
+#define SWAP16(v) ((((v)&0xff00)>>8) | (((v)&0xff)<<8))
+#define SWAP32(v) ((((v)&0xff000000ul)>>24) | (((v)&0xff0000ul)>>8) | (((v)&0xff00ul)<<8) | (((v)&0xfful)<<24))
 #define SWAPu32(v) SWAP32((u32)(v))
 #define SWAPs32(v) SWAP32((s32)(v))
 
